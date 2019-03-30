80659221bfd70392b94c6a80184cf4670df4bbb8
joe <kinjoyuji@gmail.com>
2019-03-31 01:01:48 +1100

    公式のサンプルに近づけたコードを実装

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # GCP 上にクラスタを作成
 
-まずは config.tfvar や k8s-gcp-account.json の準備をする。
+まずは config.tfvar や k8s-gcp-account.json の準備をする。以下の記事を参考。
+https://techblog.gmo-ap.jp/2017/11/16/terraform%E3%81%A7gcp%E7%92%B0%E5%A2%83%E3%82%92%E6%A7%8B%E7%AF%89%E3%81%97%E3%81%A6%E3%81%BF%E3%82%8B/
 
 workspace を移動する
 
--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,22 +1,26 @@
 variable "username" {}
 variable "password" {}
 
+# reference
+# https://www.terraform.io/docs/providers/google/r/container_cluster.html
+
 resource "google_container_cluster" "primary" {
   name                     = "k8s"
-  location                 = "us-central1-a"
+  location                 = "asia-northeast1-a"
   remove_default_node_pool = true
-  initial_node_count       = 3
+  initial_node_count       = 1
 
   master_auth {
     username = "${var.username}"
     password = "${var.password}"
   }
+}
 
-  addons_config {
-    kubernetes_dashboard {
-      disabled = true
-    }
-  }
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "k8s-node-pool"
+  location   = "asia-northeast1-a"
+  cluster    = "${google_container_cluster.primary.name}"
+  node_count = 3
 
   node_config {
     preemptible  = true
